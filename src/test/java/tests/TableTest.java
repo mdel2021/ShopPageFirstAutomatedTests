6a594b33c6ba96a60306d3a7ac349d1c5665a581
Mat Del <mateusz.dela@gmail.com>
2023-06-21 23:19:14 +0200

    table test1 and test2 completed

--- src/test/java/tests/TableTest.java
+++ src/test/java/tests/TableTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
@@ -11,14 +12,58 @@ public class TableTest extends TestBaseForTable {
 
     @Test
     public void traversingTable1() {
-        TablePage tableTest1 = new TablePage(driver, By.id("table1"));
-        int numberOfRows = tableTest1.getNumbersOfRows();
-        int numberOfColumns = tableTest1.getNumberOfColumns();
+        TablePage table = new TablePage(driver, By.id("table1"));
+        int numberOfRows = table.getNumbersOfRows();
+        int numberOfColumns = table.getNumberOfColumns();
 
         int expectedNumberOfRows = 4;
         int expectedNumberOfColumns = 6;
 
         assertEquals(numberOfRows, expectedNumberOfRows);
         assertEquals(numberOfColumns, expectedNumberOfColumns);
-        }
+
+        int firstRowIndex = 0;
+        int lastRowIndex = numberOfRows-1;
+
+        WebElement lastNameFirstRow = table.getLastNameForRow(firstRowIndex);
+        WebElement lastNameLastRow = table.getLastNameForRow(lastRowIndex);
+
+        assertEquals(lastNameFirstRow.getText(), "Smith", "Actual and expected last name value don't match");
+        assertEquals(lastNameLastRow.getText(), "Conway", "Actual and expected last name value don't match");
+
+        WebElement webSiteFirstRow = table.getWebSiteRow(firstRowIndex);
+        WebElement webSiteLastRow = table.getWebSiteRow(lastRowIndex);
+
+        assertEquals(webSiteFirstRow.getText(), "http://www.jsmith.com");
+        assertEquals(webSiteLastRow.getText(), "http://www.timconway.com");
+    }
+    @Test
+    public void traversingTable2() {
+        TablePage table = new TablePage(driver, By.id("table2"));
+        int numberOfRows = table.getNumbersOfRows();
+        int numberOfColumns = table.getNumberOfColumns();
+
+        int expectedNumberOfRows = 4;
+        int expectedNumberOfColumns = 6;
+
+        assertEquals(numberOfRows, expectedNumberOfRows);
+        assertEquals(numberOfColumns, expectedNumberOfColumns);
+
+        int firstRowIndex = 0;
+        int lastRowIndex = numberOfRows-1;
+
+        WebElement lastNameFirstRow = table.getLastNameForRow(firstRowIndex);
+        WebElement lastNameLastRow = table.getLastNameForRow(lastRowIndex);
+
+        assertEquals(lastNameFirstRow.getText(), "Smith", "Actual and expected last name value don't match");
+        assertEquals(lastNameLastRow.getText(), "Conway", "Actual and expected last name value don't match");
+
+        WebElement webSiteFirstRow = table.getWebSiteRow(firstRowIndex);
+        WebElement webSiteLastRow = table.getWebSiteRow(lastRowIndex);
+
+        assertEquals(webSiteFirstRow.getText(), "http://www.jsmith.com");
+        assertEquals(webSiteLastRow.getText(), "http://www.timconway.com");
+    }
+
+
 }
