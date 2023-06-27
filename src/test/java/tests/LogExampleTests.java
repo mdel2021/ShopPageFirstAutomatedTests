05ce6e1f5ab50ede17017a3c0edae78c86e5fcc5
Mat Del <mateusz.dela@gmail.com>
2023-06-28 00:25:28 +0200

    class browserLogExampleTest updated

--- src/test/java/tests/LogExampleTests.java
+++ src/test/java/tests/LogExampleTests.java
@@ -1,16 +1,19 @@
 package tests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
-import org.openqa.selenium.remote.CapabilityType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.logging.Level;
+
+import static org.testng.Assert.assertEquals;
 
 public class LogExampleTests {
     private WebDriver driver;
@@ -23,7 +26,7 @@ public class LogExampleTests {
         ChromeOptions chromeOptions = new ChromeOptions();
         LoggingPreferences loggingPreferences = new LoggingPreferences();
         loggingPreferences.enable(LogType.BROWSER, Level.INFO);
-        chromeOptions.setCapability(CapabilityType.LOGGING_PREFS, loggingPreferences);
+        chromeOptions.setCapability(ChromeOptions.LOGGING_PREFS, loggingPreferences);
         chromeOptions.merge(chromeOptions);
 
         driver = new ChromeDriver(chromeOptions);
@@ -33,6 +36,19 @@ public class LogExampleTests {
 
     @Test
     public void browserLogExampleTest() {
+        WebElement enterStoreLink = driver.findElement(By.cssSelector("#Content a"));
+        enterStoreLink.click();
+        WebElement signInLink = driver.findElement(By.cssSelector("#MenuContent a[href*='signonForm']"));
+        signInLink.click();
+        WebElement userNameTextField = driver.findElement(By.name("username"));
+        userNameTextField.sendKeys("NotExistingLogin");
+        WebElement passwordTextField = driver.findElement(By.name("password"));
+        passwordTextField.sendKeys("NotProperPassword");
+        WebElement loginButton = driver.findElement(By.name("signon"));
+        loginButton.click();
+        WebElement wrongLoginMessageLabel = driver.findElement(By.cssSelector("#Content ul[class='messages'] li"));
+        assertEquals("Invalid username or password. Signon failed.", wrongLoginMessageLabel.getText());
+
 
     }
 
