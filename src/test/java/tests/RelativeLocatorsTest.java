c12200945f8df59f62131103ad8b1d6dfa925ab1
Mat Del <mateusz.dela@gmail.com>
2023-07-24 23:16:24 +0200

    RelativeLocatorstest added

---
+++ src/test/java/tests/RelativeLocatorsTest.java
@@ -0,0 +1,47 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class RelativeLocatorsTest {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void beforeTest() {
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+        System.setProperty("webdriver.chrome.driver", "C:/drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+
+        driver.navigate().to("http://theinternet.przyklady.javastart.pl/login");
+    }
+
+    @Test
+    public void relativeLocatorTest() {
+        WebElement pageTitle = driver.findElement(By.tagName("h2"));
+        WebElement usernameTextField = driver.findElement(RelativeLocator.with(By.tagName("input")).below(pageTitle));
+        WebElement passwordTextField = driver.findElement(RelativeLocator.with(By.tagName("input")).below(usernameTextField));
+        WebElement loginButton = driver.findElement(RelativeLocator.with(By.tagName("button")).below(passwordTextField));
+
+        usernameTextField.sendKeys("tomsmith");
+        passwordTextField.sendKeys("SuperSecretPassword!");
+        loginButton.click();
+
+        WebElement informationAlert = driver.findElement(By.id("flash-messages"));
+
+        assertEquals(informationAlert.getText(), "You logged into a secure area!\n×");
+    }
+
+    @AfterMethod
+    public void afterTest() {
+        driver.close();
+        driver.quit();
+    }
+}
